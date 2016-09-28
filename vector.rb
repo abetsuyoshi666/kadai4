1b5674e177f7e125073a6af7d326d245cc75d2af
abetsuyoshi666 <e1202@PC4-2.cc.matsue-ct.jp>
2016-09-28 13:09:26 +0900

    新規追加プログラム

--- vector.rb
+++ vector.rb
@@ -14,6 +14,25 @@ class Vector
     c = Math.sqrt(a+b) 
     puts "#{c}" 
   end
+
+  def x
+    @x
+  end
+  
+  def y
+    @y
+  end
+
+  def add(v)
+    a = @x + v.x
+    b = @y + v.y
+    Vector.new(a, b)
+
+  end
 end
 
+v1 = Vector.new(1, 2)
+v2 = Vector.new(3, 4)
+v3 = v1.add(v2)
 
+puts v3.to_s
